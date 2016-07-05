2e68c75199b49fc11eb6a0e04309676697e84dba
Sanaeon <leon_logic@126.com>
2016-07-05 20:54:39 +0800

    edit tuil.js

--- task0002/js/util.js
+++ task0002/js/util.js
@@ -215,7 +215,81 @@ function isMobilePhone(phone) {
 
 
 //task3.1
-// 为element增加一个样式名为newClassName的新样式
+// 检查元素是否已存在className
+function hasClass(element,className) {
+	var name = element.className.match(/\s+/g) || [];
+	if (name.indexOf(className) !== -1) {
+		return true;
+	}
+	else return false;
+}
+
+//为element增加一个样式名为newClassName的新样式
 function addClass(element, newClassName) {
+    if (!hasClass(element,className)) {
+    	element.className = trim(element.className + " " + newClassName);
+    }
+}
+
+//移除element中的样式oldClassName
+function removeClass(element, oldClassName) {
+    if (hasClass(element,className)) {
+    	element.className = trim(element.className.replace(oldClassName,""));
+    }
+}
+
+// 判断siblingNode和element是否为同一个父元素下的同一级的元素，返回bool值
+function isSiblingNode(element, siblingNode) {
+    return element.parentNode === siblingNode.parentNode;
+}
+
+
+// 获取element相对于浏览器窗口的位置，返回一个对象{x, y}
+function getPosition(element) {
+	var x = 0;
+	var y = 0;
+	var current = element;
+	//获取元素的相对位置
+	while (current != null) {
+		x += current.offsetLeft;
+		y += current.offsetTop;
+		current = current.offsetParent;
+	}
+	//滚动条滚动的距离
+	var scrollLeft = document.body.scrollLeft + document.documentElement.scrollLeft;
+	var scrollTop = document.body.scrollTop + document.documentElement.scrollTop;
+	
+	//获取元素的相对位置
+	x -= screenLeft;
+	y -= scrollTop;
+	
+	return {
+		x:x;
+		y:y;
+	}
+}
+
+//
+// 实现一个简单的Query
+/*
+ 可以通过id获取DOM对象，通过#标示，例如
+$("#adom");  返回id为adom的DOM对象
+
+ 可以通过tagName获取DOM对象，例如
+$("a");  返回第一个<a>对象
+
+ 可以通过样式名称获取DOM对象，例如
+$(".classa");  返回第一个样式定义包含classa的对象
+
+ 可以通过attribute匹配获取DOM对象，例如
+$("[data-log]");  返回第一个包含属性data-log的对象
+
+$("[data-time=2015]");  返回第一个包含属性data-time且值为2015的对象
+
+ 可以通过简单的组合提高查询便利性，例如
+$("#adom .classa");  返回id为adom的DOM所包含的所有子节点中，第一个样式定义包含classa的对象
+*/
+
+function $(selector) {
     
 }
