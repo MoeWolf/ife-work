49f24080311deb0d9003c5ae499ddd68b6743e4b
Sanaeon <leon_logic@126.com>
2016-09-30 13:12:44 +0800

    edit task0002_5.js & query.js complete eventSelector

--- task0002/js/task0002_5.js
+++ task0002/js/task0002_5.js
@@ -1,31 +1,58 @@
 
 
 //事件函数
-function dragStart () {
+function dragStart (e) {
+	var dragWrap = $('#dragContainer').offsetLeft;
+	//console.log(dragWrap);
+	e = e || window.event;
+	var parent = this.parentNode;
+	startX = e.clientX;
+	startY = e.clientY;
+	startTop = parseInt(this.style.top) + 18;
+	startLeft = parent.offsetLeft - dragWrap + 80;
+	this.style.zIndex = 1;
+	moveDrag(nextDrag(this),-36);
+	console.log(dragWrap);
+	console.log($('#dragContainer').offsetParent.tagName);
+	console.log(parent.parentNode.tagName);
+}
+
+function dragging (ele) {
+	if(this.className !== 'draging') {
+		this.className = 'draging';
+	}
+}
+
+function dragOver (e) {
+	e.preventDefault();
+}
+
+function drop (e) {
 	
 }
 
-function dragging () {
-	
+//工具函数
+function nextDrag (ele) {
+	var brother = ele.nextSibling;
+	while(brother && brother.nodeName === '#text') {
+		brother = nextDrag(brother);
+	}
+	return brother;
 }
 
-function dragOver () {
-	
+function moveDrag(ele,distance) {
+	while(ele) {
+		ele.style.top = parseInt(ele.style.top) + distance + 'px';
+		ele = nextDrag(ele);
+	}
 }
-
-function drop () {
-	
-}
-
-
-
 
 
 window.onload=function () {
 	var drag = document.getElementsByClassName('drag');
 	for(var i=0,len=drag.length;i<len;i++) {
 		drag[i].draggable = true;
-		drag[i].style.top = (i%6*35) + 'px';
+		drag[i].style.top = (i%6*36) + 'px';
 		$.on(drag[i],'dragstart',dragStart);
 		$.on(drag[i],'drag',dragging);
 	}
